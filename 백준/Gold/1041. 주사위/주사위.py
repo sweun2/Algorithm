68ca032b374858d9b403b9665f5c17d188a97014
sweun2 <46338536+sweun2@users.noreply.github.com>
2022-04-13 22:54:16 +0900

    [Gold V] Title: 주사위, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1041. 주사위/주사위.py
@@ -0,0 +1,32 @@
+import sys
+
+n=int(input())
+arr=list(map(int,input().split()))
+
+
+if(n!=1):
+    result= ((n**2-3*n+2)*4 + (n-2)**2)*min(arr)
+
+    min_two=sys.maxsize
+    for i in range(5):
+        for j in reversed(range(i+1,6)):
+            if(i+j)!=5:
+                if min_two>(arr[i]+arr[j]):
+                    min_two=arr[i]+arr[j]
+
+    result+= ((n-2)*min_two)*4+((n-1)*min_two)*4
+
+    min_third=sys.maxsize
+
+    for i in range(4):
+        for j in range(i+1,5):
+            for k in range(j+1,6):
+                if(i+j)!=5 and (i+k)!=5 and(k+j)!=5:
+                    if (arr[i]+arr[j]+arr[k])<min_third:
+                        min_third=arr[i]+arr[j]+arr[k]
+
+    result += min_third*4
+
+    print(result)
+else:
+    print(sum(arr)-max(arr))
